c99d8ffd7547aa7ab9998f5866b30b949283859b
luan-brav0 <luan-brav0@github.com>
2025-04-11 11:54:29 -0300

    signup: Try-catch mongo client and fix / to \ | Capitalize PASSWORD_DEFAULT | Fix method=POST typo

--- usersapp/signup.php
+++ usersapp/signup.php
@@ -1,23 +1,24 @@
 <?php
 require 'vendor/autoload.php';
-$client = new MongoDB/Client("mongodb://localhost:27017");
-$users = $client->usersapp->users;
-
+try {
+    $client = new MongoDB\Client("mongodb://localhost:27017");
+    $users = $client->usersapp->users;
+} catch (Exception $e) {
+    die("MongoDB connection failed: " . $e->getMessage());
+}
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
-    $password = password_hash($_POST['password'], password_default);
-
-    $exists = $users->findOne(['email'=>$email]);
-    if ($exists) {
-        echo "{$email} already exists";
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $user = $users->findOne(['email'=>$email]);
+    if ($user) {
+        echo "User with email {$email} already exists";
     } else {
         $users->insertOne(['email'=>$email, 'password'=>$password]);
-        echo "Sign up {$email}"
+        echo "Sign up {$email}";
     }
 }
 ?>
-
-<form mothod="POST">
+<form method="POST">
     Email: <input name="email" type="email" required><br>
     Password: <input name="password" type="password" required><br>
     <button type="submit">Sign Up</button>
