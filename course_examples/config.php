e387fb329d29e0677837a641c59b5b70678c64b9
luan-brav0 <luan-brav0@github.com>
2025-04-21 03:35:02 -0300

    Add: Course Example files

---
+++ course_examples/config.php
@@ -0,0 +1,4 @@
+﻿<?php
+// função que conecta o PHP com o MongoDB
+// parametros: mongodb://<nome-usuário>:<senha-usuário>@<IP-hospedagem-MongoDB>:<porta-acesso>/<nome-banco-de-dados-mongodb>
+$conn = new MongoDB\Driver\Manager('mongodb://admin:pass@localhost:27017/classedb');
